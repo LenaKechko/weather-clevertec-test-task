4983dd49e9adaeee135ee5b6796da43575a8353d
Elena Kechko <ekechko@gmail.com>
2023-12-27 12:51:52 +0300

    Init commit

---
+++ src/main/java/ru/clevertec/weathertesttask/WeatherTestTaskApplication.java
@@ -0,0 +1,18 @@
+package ru.clevertec.weathertesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import ru.clevertec.weathertesttask.config.YandexConfig;
+
+@SpringBootApplication
+@EnableFeignClients
+@EnableConfigurationProperties(YandexConfig.class)
+public class WeatherTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherTestTaskApplication.class, args);
+    }
+
+}
